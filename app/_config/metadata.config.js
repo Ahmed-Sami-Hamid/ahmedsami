2c7ed646bb05b3bacb86efd3c8ced20e818e54c0
Ahmed Sami Hamid <Ahmedsamihamid1@gmail.com>
2024-09-29 16:52:25 +0400

    add the hero section

---
+++ app/_config/metadata.config.js
@@ -0,0 +1,45 @@
+// /** @type {import('next').Metadata} */
+// export const rootMetadata = {
+
+//   title: {
+//     template: '%s | Ahmed Sami',
+//     default: 'Ahmed Sami • Freelance Designer & Developer',
+//   },
+//   description:
+//     'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+//   generator: 'Ahmed Sami',
+//   applicationName: 'Ahmed Sami',
+//   referrer: 'origin-when-cross-origin',
+//   keywords: ['Design', 'Develope', 'Freelance'],
+//   authors: [
+   
+//   ],
+//   creator: 'Ali Bagheri',
+//   publisher: 'Ali Bagheri',
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: 'Ahmed Sami',
+//     description:
+//       'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+//     siteId: '1467726470533754880',
+//     creator: '@AliBagheri2079',
+//     creatorId: '1467726470533754880',
+//     images: {
+//       url: 'https://dennis-snellenberg-portfolio.vercel.app/screenshot.png',
+//       alt: 'Portfolio Screenshot',
+//     },
+//   },
+//   robots: {
+//     index: false,
+//     follow: true,
+//     nocache: true,
+//     googleBot: {
+//       index: true,
+//       follow: false,
+//       noimageindex: true,
+//       'max-video-preview': -1,
+//       'max-image-preview': 'large',
+//       'max-snippet': -1,
+//     },
+//   },
+// };
---
+++ app/_layout/header/index.jsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { MoveDownRight } from 'lucide-react';
+import { CldImage } from 'next-cloudinary';
+import AhmedsamiImg from './sami.png';
+
+import { ParallaxSlider } from '@/components';
+
+import { slideUp } from './variants';
+import Image from 'next/image';
+
+export function Header() {
+  return (
+    <motion.header
+      className='relative h-screen overflow-hidden bg-[#4e9cbc] text-background'
+      variants={slideUp}
+      initial='initial'
+      animate='enter'
+    >
+      <Image
+        src={AhmedsamiImg}
+        className='object-cover md:scale-5 md:object-contain'
+        fill={true}
+        sizes='100vw'
+        alt='Ahmed Sami Personal Picture'
+      />
+
+      <div className='relative flex h-full flex-col justify-end gap-2 md:flex-col-reverse md:justify-normal'>
+        <div className='select-none'>
+          <h1 className='text-[max(9em,15vw)]'>
+            <ParallaxSlider repeat={4} baseVelocity={2}>
+              <span className='pe-12'>
+                Ahmed Sami
+                <span className='spacer'>—</span>
+              </span>
+            </ParallaxSlider>
+          </h1>
+        </div>
+
+        <div className='md:ml-auto'>
+          <div className='mx-10 max-md:my-12 md:mx-36'>
+            <div className='mb-4 md:mb-20'>
+              <MoveDownRight size={28} strokeWidth={1.25} />
+            </div>
+
+            <h4 className='text-[clamp(1.55em,2.5vw,2.75em)]'>
+              <span className='block'>Senior</span>
+              <span className='block'>Frontend Engineer</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </motion.header>
+  );
+}
